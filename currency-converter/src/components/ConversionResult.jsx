2f835cb5cff8d258f436b2d7d52dbbf0f9648e02
constance16 <constancejohn63@yahoo.com>
2024-12-29 01:20:10 +0000

    Peviously had an issue with the rendering, i ommited a command in main.jsx that resulted to my code not rendering but it is now rectified

--- currency-converter/src/components/ConversionResult.jsx
+++ currency-converter/src/components/ConversionResult.jsx
@@ -1,19 +1,19 @@
 
-// Conversion -> https://api.frankfurter.app/latest?amount=1&from=USD$to=INR
 
-// const convertCurrency = async () => {
-//   if (!amount) return;
-//   setConverting(true);
-//   try {
-//     const res = await fetch(
-//       `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
-//     );
-//     const data = await res.json();
 
-//     setConvertedAmount(data.rates[toCurrency] + " " + toCurrency);
-//   } catch (error) {
-//     console.error("Error Fetching", error);
-//   } finally {
-//     setConverting(false);
-//   }
-// };
+const convertCurrency = async () => {
+  if (!amount) return;
+  setConverting(true);
+  try {
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+    );
+    const data = await res.json();
+
+    setConvertedAmount(data.rates[toCurrency] + " " + toCurrency);
+  } catch (error) {
+    console.error("Error Fetching", error);
+  } finally {
+    setConverting(false);
+  }
+};
--- currency-converter/src/components/CurrencySelector.jsx
+++ currency-converter/src/components/CurrencySelector.jsx
@@ -1,72 +1,55 @@
+import React, { useState, useEffect } from 'react';
 
 const CurrencySelector = () => {
-  const [currencies, setcurrencies] = useState([]);
-  const [amount, setamount] = useState(1);
+  const [currencies, setCurrencies] = useState([]);
+  const [amount, setAmount] = useState(1);
 
-  
+  const fetchCurrencies = async () => {
+    try {
+      const res = await fetch("https://api.frankfurter.app/currencies");
+      const data = await res.json();
+      setCurrencies(data);
+    } catch (error) {
+      console.error("Error fetching", error);
+    }
+  };
 
-// Currencies -> https://api.frankfurter.app/currencies
-// const fetchCurrencies =async () => {
-//   try {
-//     const res = await fetch("https://api.frankfurter.app/currencies");
-//     const dat = await res.json()
+  useEffect(() => {
+    fetchCurrencies();
+  }, []);
 
-//     setcurrencies(data);
-//   } catch (error) {
-//     console.error("Error fetching", error)
-//   }
-// };
+  return (
+    <div className="max-w-4xl mx-auto my-auto p-5 bg-white rounded-lg">
+      <h2 className="mb-6 text-2xl font-semibold text-gray-700">
+        Currency Converter
+      </h2>
 
-// useEffect(() => {
-// fetchCurrencies();
-// }, []);
+      <div className="mt-8">
+        <label 
+          htmlFor="amount"  
+          className="block text-sm font-medium text-black mb-2">
+          Amount
+        </label>
+        
+        <input
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          type="number" 
+          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-violet-500" 
+        />
+      </div>
 
-// console.log(currencies);
+      <div className="flex justify-end mt-6">
+        <button className="w-full p-1.5 bg-violet-700 text-center text-white rounded-md hover:bg-gray-500 focus:outline-none">
+          Convert
+        </button>
+      </div>
 
-
-// Conversion -> https://api.frankfurter.app/latest?amount=1&from=USD&to=INR
-
-  return <div className="max-w-4xl mx-auto my-auto p-5 bg-white rounded-lg ">
-    <h2 className="mb-6 text-2xl font-semibold text-gray-700">
-      Currency Converter
-    </h2>
-
-    <div className="mt-8">
-      <label 
-      htmlFor="amount"  
-      className="block text-sm font-medium text-black mb-2">Amount
-      </label>
-      
-      <input
-      // value={amount}
-      // onChange={(e) => setamount(e.target.value)}
-      type="number" 
-      className=" w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-violet-500" />
+      <div className="mt-2 text-sm font-medium text-right text-gray-900">
+        Amount: {amount} USD
+      </div>
     </div>
-
-    <div className="flex justify-end mt-6">
-      <button className="w-full p-1.5 bg-violet-700 text-center text-white rounded-md hover:bg-gray-500 focus:outline-none">Convert</button>
-    </div>
-
-    <div className="mt-2 text-sm font-medium text-right text-gray-900">
-      Amount: 899 USD
-    </div>
-  </div>
-
+  );
 }
 
-export default CurrencySelector
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default CurrencySelector;
